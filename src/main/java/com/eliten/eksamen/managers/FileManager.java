dcb29ea4fbac37beea25da6769d732bdce666e2d
DenizYil <denizyil@outlook.dk>
2020-11-27 10:18:46 +0100

    Move files

--- src/main/java/com/eliten/eksamen/managers/FileManager.java
+++ src/main/java/com/eliten/eksamen/managers/FileManager.java
@@ -1,5 +1,6 @@
-package com.eliten.eksamen;
+package com.eliten.eksamen.managers;
 
+import com.eliten.eksamen.Eliten;
 import com.eliten.eksamen.media.Genre;
 import com.eliten.eksamen.media.Media;
 import com.eliten.eksamen.media.MediaType;
