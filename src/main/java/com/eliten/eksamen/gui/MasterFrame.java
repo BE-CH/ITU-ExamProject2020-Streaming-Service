eb7b3111bc18f881be02af2760c0833b61499a7a
B E C H <mikkel.l.bech@gmail.com>
2020-12-01 15:51:03 +0100

    + added function to mainFrame

--- src/main/java/com/eliten/eksamen/gui/MasterFrame.java
+++ src/main/java/com/eliten/eksamen/gui/MasterFrame.java
@@ -8,15 +8,44 @@ public class MasterFrame extends JFrame {
     public MasterFrame() {
         super("Eliten Swag");
 
-        JPanel panel = new JPanel(new GridLayout(1, 2));
-        panel.add(new NavigationBar());
-        panel.add(new JPanel());
+        JPanel MainPanel = new JPanel(new GridLayout(1, 2));
+        JPanel navigationBar = new NavigationBar();
+        MainPanel.add(navigationBar);
 
-        add(panel);
+        MainPanel.add(new JPanel());
+
+        add(MainPanel);
 
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setSize(1600, 800);
         setResizable(false);
         setVisible(true);
+    }
+
+    public void removeView(JPanel mainPanel, JPanel panelToRemove){
+
+        mainPanel.remove(panelToRemove);
+        mainPanel.revalidate();
+        mainPanel.repaint();
+
+    }
+
+    public void showView(JPanel mainPanel, JPanel panelToShow){
+        mainPanel.add(panelToShow);
+        mainPanel.revalidate();
+        mainPanel.repaint();
+    }
+
+    public void removeAndShow(JPanel mainPanel, JPanel[] panelToRemove, JPanel[] panelToShow){
+        for (JPanel panel :
+                panelToRemove) {
+            mainPanel.remove(panel);
+        }
+        for (JPanel panel :
+                panelToShow) {
+            mainPanel.add(panel);
+        }
+        mainPanel.revalidate();
+        mainPanel.repaint();
     }
 }
